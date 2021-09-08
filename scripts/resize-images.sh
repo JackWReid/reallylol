3d5571ec35c54863c9fc119280418b959daf5eee
Jack Reid <hello@jackreid.xyz>
2021-09-08 11:40:15 +0100

    scripting

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -euxo pipefail
 
-#find ~/Downloads/upload -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
-#find ~/Downloads/upload -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
-#find ~/Downloads/upload -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1000\>x1000\> {} +
-
-echo "[Photo Post] img:$1 filename:$2"
+echo "[Photo Post] img:$1 filename:$2 title:$3 loc:$4"
 date=$(date +"%Y-%m-%d")
 imgpath="./static/img/photo/$2.jpg"
 mdpath="./content/photo/$date.md"
 absimgpath="/img/photo/$2.jpg"
+
+# Move and transform the image file
 cp $1 $imgpath
 mogrify -verbose -format jpg -layers Dispose -resize 1000\>x1000\> -quality 80% $imgpath
+
+# Create photo .md file for Hugo
 hugo new photo/$date.md
-echo $absimgpath >> $mdpath
+touch $mdpath
+echo "---" >> $mdpath
+echo "title: \"$3\"" >> $mdpath
+echo "date: $date" >> $mdpath
+echo "location: $4" >> $mdpath
+echo "image: \"$absimgpath\"" >> $mdpath
+echo "---\n" >> $mdpath
+echo "![]($absimgpath)\n" >> $mdpath
 vim $mdpath
