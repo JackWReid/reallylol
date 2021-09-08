ae9bd4c1f796794c1b8eaa4e933fc96247ddb4ab
Jack Reid <hello@jackreid.xyz>
2021-09-08 12:47:58 +0100

    scripts

---
+++ scripts/new-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "[PHOTO] img:$1 filename:$2 title:$3 loc:$4"
+date=$(date +"%Y-%m-%d")
+imgpath="./static/img/photo/$2.jpg"
+mdpath="./content/photo/$date.md"
+absimgpath="/img/photo/$2.jpg"
+
+# Move and transform the image file
+# brew install imagemagick if mogrify is missing
+cp $1 $imgpath
+mogrify -quiet -format jpg -layers Dispose -resize 1000\>x1000\> -quality 80% $imgpath
+
+# Create photo .md file for Hugo
+touch $mdpath
+echo "---" >> $mdpath
+echo "title: \"$3\"" >> $mdpath
+echo "date: $date" >> $mdpath
+echo "location: $4" >> $mdpath
+echo "image: \"$absimgpath\"" >> $mdpath
+echo "---" >> $mdpath
+echo "" >> $mdpath
+echo "![]($absimgpath)" >> $mdpath
+vim $mdpath
