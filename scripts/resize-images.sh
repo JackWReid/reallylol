95367bbe3c688b54398560d871e3a6831b70ad11
Jack Reid <hello@jackreid.xyz>
2021-09-08 11:33:52 +0100

    scripting

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -6,10 +6,12 @@ set -euxo pipefail
 #find ~/Downloads/upload -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1000\>x1000\> {} +
 
 echo "[Photo Post] img:$1 filename:$2"
-imgpath="./assets/img/photo/$2"
-mdpath="./content/photo/$date.md"
 date=$(date +"%Y-%m-%d")
+imgpath="./static/img/photo/$2.jpg"
+mdpath="./content/photo/$date.md"
+absimgpath="/img/photo/$2.jpg"
 cp $1 $imgpath
-mogrify -verbose -format jpeg -layers Dispose -resize 1000\>x1000\> -quality 80% $imgpath
+mogrify -verbose -format jpg -layers Dispose -resize 1000\>x1000\> -quality 80% $imgpath
 hugo new photo/$date.md
+echo $absimgpath >> $mdpath
 vim $mdpath
