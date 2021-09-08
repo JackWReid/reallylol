0d164d1d8c96cde4f5ebfd4c3f0b2a7713d0323f
Jack Reid <hello@jackreid.xyz>
2021-09-08 11:27:06 +0100

    scripting

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-find ~/Downloads/upload -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
-find ~/Downloads/upload -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
-find ~/Downloads/upload -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1000\>x1000\> {} +
+set -euxo pipefail
+
+#find ~/Downloads/upload -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
+#find ~/Downloads/upload -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1000\>x1000\> -quality 75% {} +
+#find ~/Downloads/upload -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1000\>x1000\> {} +
+
+echo "[Photo Post] img:$1 filename:$2"
+imgpath="./assets/img/photo/$2"
+mdpath="./content/photo/$date.md"
+date=$(date +"%Y-%m-%d")
+cp $1 $imgpath
+mogrify -verbose -format jpeg -layers Dispose -resize 1000\>x1000\> -quality 80% $imgpath
+hugo new photo/$date.md
+vim $mdpath
