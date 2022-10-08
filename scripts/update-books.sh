0a61973d3661e9d142266c159780b241d410866d
Jack Reid <hello@jackreid.xyz>
2022-10-08 13:07:03 +0100

    Update book update scripts
    Book update scripts now rely on `okudl`, a local machine binary
    that downloads and parses RSS feeds from Oku to produce Hugo-
    -compatible JSON.

--- scripts/update-books.sh
+++ scripts/update-books.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Updating books pages from Goodreads export"
-echo "https://www.goodreads.com/review/import for latest CSV"
-mkdir ./tmp
-cp $1 ./tmp/raw.csv
-
-/usr/local/opt/sqlite/bin/sqlite3 < ./scripts/update-books.sql
-
-mv ./tmp/toread.json ./data/books/toread.json
-mv ./tmp/reading.json ./data/books/reading.json
-mv ./tmp/read.json ./data/books/read.json
-rm -r ./tmp
+okudl toread > ./data/books/toread.json
+okudl reading > ./data/books/reading.json
+okudl read > ./data/books/read.json
