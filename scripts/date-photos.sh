2642e73969a0bf4949cb7229992a107c1a646332
Jack Reid <jack.r@miro.com>
2023-02-08 15:39:11 +0100

    move image script fix

--- scripts/date-photos.sh
+++ scripts/date-photos.sh
@@ -5,21 +5,21 @@ set -euo pipefail
 mddir="./content/photo"
 imgdir="./static/img/photo"
 
-#for mdfile in $mddir/*.md; do
-#	filedate=$(grep "^date: " $mdfile | cut -c 7-16)
-#	mdfilebase=$(basename $mdfile)
-#
-#	if [[ "$mdfilebase" =~ ^$filedate.* ]];
-#	then
-#  	echo "[MATCH] $mdfilebase : $filedate"
-#	else
-#		echo "[MISS] $mdfilebase : $filedate"
-#		mv $mdfile "$mddir/$filedate-$mdfilebase"
-#	fi
-#done
-
-for mdfile in $mddir/2022-07-27*; do
+for mdfile in $mddir/*.md; do
+	filedate=$(grep "^date: " $mdfile | cut -c 7-16)
 	mdfilebase=$(basename $mdfile)
-	nodatename=$(echo $mdfilebase | cut -c 12-)
-	mv $mdfile $mddir/2022-07-22-$nodatename
+
+	if [[ "$mdfilebase" =~ ^$filedate.* ]];
+	then
+  	echo "[MATCH] $mdfilebase : $filedate"
+	else
+		echo "[MISS] $mdfilebase : $filedate"
+		mv $mdfile "$mddir/$filedate-$mdfilebase"
+	fi
 done
+
+#for mdfile in $mddir/2022-07-27*; do
+#	mdfilebase=$(basename $mdfile)
+#	nodatename=$(echo $mdfilebase | cut -c 12-)
+#	mv $mdfile $mddir/2022-07-22-$nodatename
+#done
