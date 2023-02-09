b2b8e9c3b22d99fc8ebeed18e884b4fb0f3da607
Jack Reid <jack.r@miro.com>
2023-02-09 11:38:19 +0100

    move some files

--- scripts/date-photo.sh
+++ scripts/date-photo.sh
@@ -6,8 +6,11 @@ imgdir="./static/img/photo"
 imgbase=$(basename $1)
 imgcode=${imgbase::${#imgbase}-4}
 
+echo $imgbase $imgcode $mdfile
 mdfile=$(find $mddir -name "*$imgcode*")
+if [ -z "${mdfile}" ]; exit 1
+
 filedate=$(grep "^date: " $mdfile | cut -c 7-16)
-echo "[MOVE] $imgbase : $filedate"
+echo "[FOUND] $imgbase : $filedate"
 sed -i '' "s|$imgcode|$filedate-$imgcode|g" $mdfile
 mv $1 $imgdir/$filedate-$imgbase
