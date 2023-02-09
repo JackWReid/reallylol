2accdf5e00979a1172ec0a8d39e4bdadea2b7a27
Jack Reid <jack.r@miro.com>
2023-02-09 11:24:28 +0100

    single move photo script

---
+++ scripts/date-photo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+set -euo pipefail
+
+mddir="./content/photo"
+imgdir="./static/img/photo"
+imgbase=$(basename $1)
+imgcode=${imgbase::${#imgbase}-4}
+
+mdfile=$(find $mddir -name "*$imgcode*")
+filedate=$(grep "^date: " $mdfile | cut -c 7-16)
+echo "[MOVE] $imgbase : $filedate"
+sed -i '' "s|$imgcode|$filedate-$imgcode|g" $mdfile
+mv $1 $imgdir/$filedate-$imgbase
