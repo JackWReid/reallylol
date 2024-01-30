1beda2585d56e7140dedc2d4b7523f8a05610552
Jack Reid <jack.r@miro.com>
2024-01-30 14:08:42 +0100

    format book data on output

--- scripts/update-books.sh
+++ scripts/update-books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-okudl toread > ./data/books/toread.json
-okudl reading > ./data/books/reading.json
-okudl read > ./data/books/read.json
+okudl toread | jq . > ./data/books/toread.json
+okudl reading | jq . > ./data/books/reading.json
+okudl read | jq . > ./data/books/read.json
