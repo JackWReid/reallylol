b82a61c18883f62ab9efe0cde1232682e97d1825
Jack Reid <jack.r@miro.com>
2025-07-18 10:08:40 +0200

    make note names consistent

--- scripts/new-note.sh
+++ scripts/new-note.sh
@@ -3,8 +3,17 @@ set -euo pipefail
 
 read -p "Note: " note
 
+# Generate slug from note title
+slug=$(echo "$note" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9 ]//g' | sed 's/ /-/g' | sed 's/--*/-/g' | sed 's/^-\|-$//g' | cut -c1-50)
+
+# If slug is empty, use "untitled"
+if [ -z "$slug" ]; then
+    slug="untitled"
+fi
+
+date_only=$(date +"%Y-%m-%d")
 date_time=$(date +"%Y-%m-%dT%H:%M:%S")
-md_path="./content/note/$date_time.md"
+md_path="./content/note/$date_only-$slug.md"
 
 md_template=$(cat <<EOF
 ---
