52171dc959aad8aac8fe924c1e1a00a0398115a0
Jack Hackinger-Reid <jack@hackinger-reid.eu>
2026-04-04 16:31:58 +0200

    scaffold: initialise Astro project in astro/

---
+++ astro/astro.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://really.lol",
+  output: "static",
+  integrations: [
+    sitemap({
+      serialize(item) {
+        if (item.url === "https://really.lol/") item.priority = 1.0;
+        else if (item.url.startsWith("https://really.lol/post")) item.priority = 0.6;
+        else if (item.url.startsWith("https://really.lol/photo")) item.priority = 0.5;
+        else if (item.url.startsWith("https://really.lol/note")) item.priority = 0.3;
+        return item;
+      },
+    }),
+  ],
+  markdown: {
+    shikiConfig: { theme: "dracula" },
+  },
+});
